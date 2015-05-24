6d861e2435396834059d91c87729f887ca186d73
Hiwa Hirokazu <niwacchi@gmail.com>
2015-05-24 19:00:43 +0900

    Create sample of ASP.NET WebForms with NUnit. But pending...

---
+++ AspNetWFTest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetWFTest.Startup))]
+namespace AspNetWFTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
